fix multi-gpu training bugs and add normalization

maniskill2_learn/networks/modules/cnn_modules/multi_image_obs_encoder.py
@@ -5,7 +5,6 @@ import torch
 import torch.nn as nn
 import torchvision
 from .crop_randomizer import CropRandomizer
-from maniskill2_learn.utils.diffusion.module_attr_mixin import ModuleAttrMixin
 from maniskill2_learn.utils.diffusion.torch import dict_apply, replace_submodules
 from maniskill2_learn.networks.modules.cnn_modules.model_getter import get_resnet
 from maniskill2_learn.networks.backbones.rl_cnn import CNNBase
@@ -13,7 +12,7 @@ from maniskill2_learn.utils.torch import no_grad
 from maniskill2_learn.networks.builder import MODELNETWORKS
 
 @MODELNETWORKS.register_module()
-class MultiImageObsEncoder(ModuleAttrMixin, CNNBase):
+class MultiImageObsEncoder(CNNBase):
     @no_grad
     def preprocess(self, inputs):
         # assert inputs are channel-first; output is channel-first
@@ -72,7 +71,6 @@ class MultiImageObsEncoder(ModuleAttrMixin, CNNBase):
             for key in obs_shape_meta.keys():
                 key_model_map[key] = rgb_model
 
-        print(obs_shape_meta)
         for key, attr in obs_shape_meta.items():
             shape = tuple(attr['shape'])
             key_shape_map[key] = shape
@@ -122,7 +120,6 @@ class MultiImageObsEncoder(ModuleAttrMixin, CNNBase):
 
                 # configure randomizer
                 this_randomizer = nn.Identity()
-                print(input_shape)
                 if crop_shape is not None:
                     if isinstance(crop_shape, dict):
                         h, w = crop_shape[key]

maniskill2_learn/utils/diffusion/mask_generator.py
@@ -1,7 +1,7 @@
 from typing import Sequence, Optional
 import torch
 from torch import nn
-from .module_attr_mixin import ModuleAttrMixin
+from maniskill2_learn.utils.torch import ExtendedModule
 
 
 def get_intersection_slice_mask(
@@ -29,7 +29,7 @@ def get_union_slice_mask(
     return mask
 
 
-class DummyMaskGenerator(ModuleAttrMixin):
+class DummyMaskGenerator(ExtendedModule):
     def __init__(self):
         super().__init__()
     
@@ -40,7 +40,7 @@ class DummyMaskGenerator(ModuleAttrMixin):
         return mask
 
 
-class LowdimMaskGenerator(ModuleAttrMixin):
+class LowdimMaskGenerator(ExtendedModule):
     def __init__(self,
         action_dim, obs_dim,
         # obs mask setup
@@ -59,8 +59,8 @@ class LowdimMaskGenerator(ModuleAttrMixin):
         self.return_one_mask = return_one_mask
 
     @torch.no_grad()
-    def forward(self, shape, seed=None):
-        device = self.device
+    def forward(self, shape, device, seed=None):
+        # device = self.device
         B, T, D = shape
         assert D == (self.action_dim + self.obs_dim)
 
@@ -112,7 +112,7 @@ class LowdimMaskGenerator(ModuleAttrMixin):
         return action_mask, obs_mask
 
 
-class KeypointMaskGenerator(ModuleAttrMixin):
+class KeypointMaskGenerator(ExtendedModule):
     def __init__(self, 
             # dimensions
             action_dim, keypoint_dim,

maniskill2_learn/utils/diffusion/normalizer.py
@@ -265,9 +265,10 @@ def _normalize(x, params, forward=True):
     assert 'scale' in params
     if isinstance(x, np.ndarray):
         x = torch.from_numpy(x)
-    scale = params['scale']
-    offset = params['offset']
-    x = x.to(device=scale.device, dtype=scale.dtype)
+        x = x.to(device=scale.device, dtype=scale.dtype)
+    scale = params['scale'].to(device=x.device, dtype=x.dtype)
+    offset = params['offset'].to(device=x.device, dtype=x.dtype)
+    # x = x.to(device=scale.device, dtype=scale.dtype)
     src_shape = x.shape
     x = x.reshape(-1, scale.shape[0])
     if forward: