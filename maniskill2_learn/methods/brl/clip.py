7a2eee03467ee49bc709d3eb54498c04eac6cc29
zbzhu99 <zbzhu99@icloud.com>
2023-07-09 06:08:24 +0000

    Fix clip loss

--- maniskill2_learn/methods/brl/clip.py
+++ maniskill2_learn/methods/brl/clip.py
@@ -19,7 +19,7 @@ from ..builder import BRL
 
 def cross_entropy(preds, targets, reduction="none"):
     log_softmax = nn.LogSoftmax(dim=-1)
-    loss = (-targets * log_softmax(preds)).sum(1)
+    loss = (-targets * log_softmax(preds)).mean(1)
     if reduction == "none":
         return loss
     elif reduction == "mean":
