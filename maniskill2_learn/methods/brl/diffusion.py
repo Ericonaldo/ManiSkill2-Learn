a946d869395e8c14090dee9eade3686234f2b0ad
Ericonaldo <ericliuof97@gmail.com>
2023-07-25 16:14:14 +0800

    only diffuse tcppose and action pose

--- maniskill2_learn/methods/brl/diffusion.py
+++ maniskill2_learn/methods/brl/diffusion.py
@@ -153,7 +153,8 @@ class DiffAgent(BaseAgent):
         if not obs_as_global_cond:
             obs_dim = self.obs_feature_dim
         if diffuse_state:
-            obs_dim = self.state_dim
+            # obs_dim = self.state_dim
+            obs_dim = 7 # We only diffuse tcp pose
         self.mask_generator = LowdimMaskGenerator(
             action_dim=self.action_dim,
             obs_dim=obs_dim,
